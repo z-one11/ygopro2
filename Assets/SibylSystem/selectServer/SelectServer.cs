335d908b5ca8accebc5624ffe4610e16ee480344
無名の凝泪 <wumeng170@gmail.com>
2019-06-05 13:14:52 +0800

    add serversList

--- Assets/SibylSystem/selectServer/SelectServer.cs
+++ Assets/SibylSystem/selectServer/SelectServer.cs
@@ -7,11 +7,15 @@ using System.Threading;
 public class SelectServer : WindowServantSP
 {
     UIPopupList list;
+    UIPopupList serversList;
 
     UIInput inputIP;
     UIInput inputPort;
     UIInput inputPsw;
     UIInput inputVersion;
+
+    UISprite inputIP_;
+    UISprite inputPort_;
 
     public override void initialize()
     {
@@ -19,16 +23,94 @@ public class SelectServer : WindowServantSP
         UIHelper.registEvent(gameObject, "exit_", onClickExit);
         UIHelper.registEvent(gameObject, "face_", onClickFace);
         UIHelper.registEvent(gameObject, "join_", onClickJoin);
-        UIHelper.getByName<UIInput>(gameObject, "name_").value = Config.Get("name","一秒一喵机会");
+        UIHelper.registEvent(gameObject, "clearPsw_", onClearPsw);
+        serversList = UIHelper.getByName<UIPopupList>(gameObject, "server");
+        UIHelper.registEvent(gameObject, "server", pickServer);
+        UIHelper.getByName<UIInput>(gameObject, "name_").value = Config.Get("name", "YGOPro2 User");
         list = UIHelper.getByName<UIPopupList>(gameObject, "history_");
-        UIHelper.registEvent(gameObject,"history_", onSelected);
-        name = Config.Get("name", "一秒一喵机会");
+        UIHelper.registEvent(gameObject, "history_", onSelected);
+        name = Config.Get("name", "YGOPro2 User");
         inputIP = UIHelper.getByName<UIInput>(gameObject, "ip_");
         inputPort = UIHelper.getByName<UIInput>(gameObject, "port_");
         inputPsw = UIHelper.getByName<UIInput>(gameObject, "psw_");
+        inputIP_ = UIHelper.getByName<UISprite>(gameObject, "ip_");
+        inputPort_ = UIHelper.getByName<UISprite>(gameObject, "port_");
         inputVersion = UIHelper.getByName<UIInput>(gameObject, "version_");
         set_version("0x" + String.Format("{0:X}", Config.ClientVersion));
+
+        inputIP.value = Config.Get("ip_", "s1.ygo233.com");
+        inputPort.value = Config.Get("port_", "233");
+        serversList.value = Config.Get("serversPicker", "[Ser1] 233正式服");
+
+        serversList.items.Add("[Ser1] 233正式服");
+        serversList.items.Add("[Ser2] 23333先行服");
+        serversList.items.Add("[Ser3] 2Pick轮抽服");
+        serversList.items.Add("[Ser4] Koishi Server (TCG)");
+        serversList.items.Add("[自定义]");
+
         SetActiveFalse();
+    }
+
+    private void pickServer()
+    {
+        string server = serversList.value;
+        switch (server)
+        {
+            case "[Ser1] 233正式服":
+            {
+                UIHelper.getByName<UIInput>(gameObject, "ip_").value = "s1.ygo233.com";
+                UIHelper.getByName<UIInput>(gameObject, "port_").value = "233";
+                Config.Set("serversPicker", "[Ser1] 233正式服");
+                save();
+
+                inputIP_.enabled = false;
+                inputPort_.enabled = false;
+                break;
+            }
+            case "[Ser2] 23333先行服":
+            {
+                UIHelper.getByName<UIInput>(gameObject, "ip_").value = "s1.ygo233.com";
+                UIHelper.getByName<UIInput>(gameObject, "port_").value = "23333";
+                Config.Set("serversPicker", "[Ser2] 23333先行服");
+                save();
+
+                inputIP_.enabled = false;
+                inputPort_.enabled = false;
+                break;
+            }
+            case "[Ser3] 2Pick轮抽服":
+            {
+                UIHelper.getByName<UIInput>(gameObject, "ip_").value = "2pick.mycard.moe";
+                UIHelper.getByName<UIInput>(gameObject, "port_").value = "765";
+                Config.Set("serversPicker", "[Ser3] 2Pick轮抽服");
+                save();
+
+                inputIP_.enabled = false;
+                inputPort_.enabled = false;
+                break;
+            }
+            case "[Ser4] Koishi Server (TCG)":
+            {
+                UIHelper.getByName<UIInput>(gameObject, "ip_").value = "koishi.moecube.com";
+                UIHelper.getByName<UIInput>(gameObject, "port_").value = "1311";
+                Config.Set("serversPicker", "[Ser4] Koishi Server (TCG)");
+                save();
+
+                inputIP_.enabled = false;
+                inputPort_.enabled = false;
+                break;
+            }
+            default:
+            {
+                Config.Set("serversPicker", "[自定义]");
+                save();
+
+                inputIP_.enabled = true;
+                inputPort_.enabled = true;
+                break;
+            }
+        }
+
     }
 
     void onSelected()
@@ -41,6 +123,7 @@ public class SelectServer : WindowServantSP
 
     private void readString(string str)
     {
+/*
         str = str.Substring(1, str.Length - 1);
         string version = "", remain = "";
         string[] splited;
@@ -76,7 +159,24 @@ public class SelectServer : WindowServantSP
         inputIP.value = ip;
         inputPort.value = port;
         inputPsw.value = psw;
+*/
+        str = str.Substring(5, str.Length - 5);
+        inputPsw.value = str;
         //inputVersion.value = version;
+    }
+
+    void onClearPsw()
+    {
+        string PswString = File.ReadAllText("config/passwords.conf");
+        string[] lines = PswString.Replace("\r", "").Split("\n");
+        for (int i = 0; i < lines.Length; i++)
+        {
+            list.RemoveItem(lines[i]);//清空list
+        }
+        FileStream stream = new FileStream("config/passwords.conf", FileMode.Truncate, FileAccess.ReadWrite);//清空文件内容
+        stream.Close();
+        inputPsw.value = "";
+        Program.PrintToChat(InterString.Get("房间密码已清空"));
     }
 
     public override void show()
@@ -96,11 +196,11 @@ public class SelectServer : WindowServantSP
     void printFile(bool first)
     {
         list.Clear();
-        if (File.Exists("config/hosts.conf") == false)
+        if (File.Exists("config/passwords.conf") == false)
         {
-            File.Create("config/hosts.conf").Close();
+            File.Create("config/passwords.conf").Close();
         }
-        string txtString = File.ReadAllText("config/hosts.conf");
+        string txtString = File.ReadAllText("config/passwords.conf");
         string[] lines = txtString.Replace("\r", "").Split("\n");
         for (int i = 0; i < lines.Length; i++)
         {
@@ -165,7 +265,7 @@ public class SelectServer : WindowServantSP
         gameObject.SetActive(!Bool);
     }
 
-    public void KF_onlineGame(string Name,string ipString, string portString, string versionString, string pswString="")
+    public void KF_onlineGame(string Name, string ipString, string portString, string versionString, string pswString = "")
     {
         name = Name;
         Config.Set("name", name);
@@ -177,11 +277,12 @@ public class SelectServer : WindowServantSP
         {
             if (name != "")
             {
-                string fantasty = "(" + versionString + ")" + ipString + ":" + portString + " " + pswString;
+                //string fantasty = "(" + versionString + ")" + ipString + ":" + portString + " " + pswString;
+                string fantasty = "psw: " + pswString;
                 list.items.Remove(fantasty);
                 list.items.Insert(0, fantasty);
                 list.value = fantasty;
-                if (list.items.Count>5) 
+                if (list.items.Count > 5)
                 {
                     list.items.RemoveAt(list.items.Count - 1);
                 }
@@ -190,9 +291,9 @@ public class SelectServer : WindowServantSP
                 {
                     all += list.items[i] + "\r\n";
                 }
-                File.WriteAllText("config/hosts.conf", all);
+                File.WriteAllText("config/passwords.conf", all);
                 printFile(false);
-                (new Thread(() => { TcpHelper.join(ipString, name, portString, pswString,versionString); })).Start();
+                (new Thread(() => { TcpHelper.join(ipString, name, portString, pswString, versionString); })).Start();
             }
             else
             {
@@ -210,6 +311,12 @@ public class SelectServer : WindowServantSP
         name = UIHelper.getByName<UIInput>(gameObject, "name_").value;
         RMSshow_face("showFace", name);
         Config.Set("name", name);
+    }
+
+    public void save()
+    {
+        Config.Set("ip_", UIHelper.getByName<UIInput>(gameObject, "ip_").value);
+        Config.Set("port_", UIHelper.getByName<UIInput>(gameObject, "port_").value);
     }
 
 }
