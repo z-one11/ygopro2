19003cda4fded7686d1ab5b30f946c386e8c04b8
無名の凝泪 <wumeng170@gmail.com>
2019-06-06 14:30:15 +0800

    add joinQQ

---
+++ Assets/SibylSystem/Menu/MenuButton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    public Canvas buttonCanvas;
+    public CanvasGroup buttonCanvasGroup;
+    public GameObject button;
+    // Use this for initialization
+    void Start()
+    {
+        buttonCanvas = GetComponent<Canvas>();
+        buttonCanvasGroup = GetComponent<CanvasGroup>();
+        button = GameObject.Find("Button");
+    }
+
+    public void onClickJoinQQ()
+    {
+        Application.OpenURL("https://jq.qq.com/?_wv=1027&k=5YrX11n");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Program.I().menu!=null && Program.I().setting !=null && Program.I().menu.isShowed && !Program.I().setting.isShowed)
+        {
+            buttonCanvasGroup.alpha = 1f;
+            button.SetActive(true);
+        }
+        else
+        {
+            buttonCanvasGroup.alpha = 0f;
+            button.SetActive(false);
+        }
+    }
+}
