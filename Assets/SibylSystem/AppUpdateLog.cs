3f4de76dad93ffc48734a4d22fa62ef144dddb75
Unicorn369 <3038572935@qq.com>
2020-01-07 10:23:06 +0800

    version

--- Assets/SibylSystem/AppUpdateLog.cs
+++ Assets/SibylSystem/AppUpdateLog.cs
@@ -3,7 +3,7 @@ using System.IO;
 
 public class AppUpdateLog
 {
-    public static string GAME_VERSION = Program.PRO_VERSION() + "-1219";
+    public static string GAME_VERSION = Program.PRO_VERSION() + "-0107";
     //游戏数据
     public static string GAME_DATA_VERSION = "updates/ver_" + GAME_VERSION + ".txt";
     //音效文件
@@ -14,15 +14,15 @@ public class AppUpdateLog
     public static string GAME_UI_VERSION = "updates/ui.txt";
 
     //立绘数据
-    public static string GAME_CLOSEUP_VERSION = "updates/closeup_0.11.txt";//新版本
-    public static string OLD_CLOSEUP_VERSION = "updates/closeup_0.10.txt"; //旧版本
+    public static string GAME_CLOSEUP_VERSION = "updates/closeup_0.12.txt";//新版本
+    public static string OLD_CLOSEUP_VERSION = "updates/closeup_0.11.txt"; //旧版本
 
-    public static string MAIN_CLOSEUP_ZIP = "closeup_0.11.zip";    //主数据
-    public static string PATCH_CLOSEUP_ZIP = "up_closeup_0.11.zip";//更新包
+    public static string MAIN_CLOSEUP_ZIP = "closeup_0.12.zip";    //主数据
+    public static string PATCH_CLOSEUP_ZIP = "up_closeup_0.12.zip";//更新包
 
     //MD5
-    public static string MD5_MAIN_CLOSEUP = "b661703c411fea0154ec3d0c8e4eef01";  //主数据
-    public static string MD5_PATCH_CLOSEUP = "acd0d950c5bd8bbf956f61d4a66ecac4"; //更新包
+    public static string MD5_MAIN_CLOSEUP = "2e1f2b2fc9d89ffa1e82743710b7b03b";  //主数据
+    public static string MD5_PATCH_CLOSEUP = "b6db833490c7d99c0e827b4d973ce2ff"; //更新包
     public static string MD5_IMAGE = "cac9a9dc4748e838a5ba3d6aa3dc924c";         //卡图包
 
     //保留的文件
